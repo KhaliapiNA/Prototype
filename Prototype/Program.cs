595f188741865a6d5fe1beacb36c8a3dd7ed7956
SeilorMoon <nastya16.com@gmail.com>
2020-06-16 16:18:58 +0300

    Initial commit

---
+++ Prototype/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create two instances and clone each
+            Telo T1 = new Telo(10, "ggg");
+            System s1 = new ConcretePrototype1(T1);
+            System c1 = s1.Clone();
+
+            c1.SsS.DLINA = 8;
+            c1.SsS.NAME = "rrrrrr";
+
+            Console.WriteLine("Dlina: {0} Name: {1}", c1.SsS.DLINA, c1.SsS.NAME);
+
+            Console.WriteLine("Dlina: {0} Name: {1}", s1.SsS.DLINA, s1.SsS.NAME);
+
+            Telo T2 = new Telo(50, "hohoho");
+            System s2 = new ConcretePrototype2(T2);
+            System c2 = s2.Clone();
+
+            c2.SsS.DLINA = 25;
+            c2.SsS.NAME = "zozozo";
+
+            Console.WriteLine("Dlina: {0} Name: {1}", c2.SsS.DLINA, c2.SsS.NAME);
+
+            Console.WriteLine("Dlina: {0} Name: {1}", s2.SsS.DLINA, s2.SsS.NAME);
+
+
+            // Wait for user
+            Console.Read();
+        }
+    }
+}
---
+++ Prototype/Telo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    
+    class Telo
+    {
+        int dlina;
+        string name;
+
+        public Telo(int dln, string nm)
+        {
+            dlina = dln;
+            name = nm;
+        }
+
+        public int DLINA
+        {
+            get { return dlina; }
+            set { dlina= value;}
+        }
+        public string NAME
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+
+    // "System"
+
+    abstract class System
+    {
+        private Telo SSS;
+
+        // Constructor
+        public System( Telo ddd)
+        {
+            SSS = ddd;
+        }
+
+        // Property
+        public Telo SsS
+        {
+            get { return SSS; }
+            set { SSS = value; }
+        }
+
+        public abstract System Clone();
+    }
+
+    // "ConcretePrototype1"
+
+    class ConcretePrototype1 : System
+    {
+        // Constructor
+        public ConcretePrototype1(Telo ddd)
+            : base(ddd)
+        {
+        }
+
+        public override System Clone()
+        {
+            // Shallow copy
+            return (System)this.MemberwiseClone();
+        }
+    }
+
+    // "ConcretePrototype2"
+
+    class ConcretePrototype2 : System
+    {
+        // Constructor
+        public ConcretePrototype2(Telo ddd)
+            : base(ddd)
+        {
+        }
+
+        public override System Clone()
+        {
+            // Deep copy
+           
+            return new ConcretePrototype2( new Telo(SsS.DLINA, SsS.NAME));
+        }
+    }
+}
